3543226155224ee9573f3bb4e2fa8dc39ec379eb
ritick <harshit@RITICK>
2022-09-18 23:41:33 +0530

    Day 1

---
+++ Day1/bandnamegenerator.py
@@ -0,0 +1,4 @@
+print("\t\tBAND NAME GENERATOR.......\n\n")
+city=input("\tEnter Name of your city : \n\t")
+pet_name=input("\tEnter name of your pet : \n\t")
+print("\n\tName of your Band can be : "+ city + pet_name)
